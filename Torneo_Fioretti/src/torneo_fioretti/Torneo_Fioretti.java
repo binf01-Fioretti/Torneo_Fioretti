0efc05a467ac379f0743476f8c90d80762c6c7a9
Fioretti Daniele <fiorettidaniele9@gmail.com>
2024-12-10 17:53:26 +0100

    aggiunto nuovo progetto

---
+++ Torneo_Fioretti/src/torneo_fioretti/Torneo_Fioretti.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package torneo_fioretti;
+
+/**
+ *
+ * @author Daniele
+ */
+public class Torneo_Fioretti {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
